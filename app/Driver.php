b3718ca5585d63c63c6f6c62671d25fc80e90b4d
William Nwogbo <williamnwogbo@gmail.com>
2018-07-27 15:52:52 +0100

    check

--- app/Driver.php
+++ app/Driver.php
@@ -2,13 +2,17 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Geographical;
-class Driver extends Authenticatable
+
+class Driver extends Model
 {
     use Notifiable;
+
     protected static $kilometers = true;
+
     public $guarded = [];
 
     public function truck(){
